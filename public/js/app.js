4f19e71180f10d86eb299bbf74e6842d8d9140f1
saurot123 <sk.saurot@gmail.com>
2020-09-23 23:22:01 +0530

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search=document.querySelector('input');
 const message1=document.querySelector('#message-1');
 const message2=document.querySelector('#message-2');
 
-
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location=search.value;
